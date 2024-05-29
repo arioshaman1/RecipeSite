d9fb9e23d3009c7e3c3bf4474dbf9a4e90b8f1a8
Андрей <adaikin.andr321@gmail.com>
2024-05-29 08:40:01 +0300

    final for vadim

--- obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aeb86eccd1802855edc3ac0906cf13cd4a38b234")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c5a615c03c8d72d5213bf7eeb3c53fe7c289663")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
