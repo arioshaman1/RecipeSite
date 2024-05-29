0c5a615c03c8d72d5213bf7eeb3c53fe7c289663
Андрей <adaikin.andr321@gmail.com>
2024-05-29 03:33:20 +0300

    final

--- Model/User.cs
+++ Model/User.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace WebApplication2.Model;
 
 public class User
 {
     public int ID { get; set; }
-    public string Name  { get; set; }
-    public string Email { get; set; }
+    public string Recept  { get; set; }
+    public string Description { get; set; }
+    public string Type { get; set; }
+    public string Photo { get; set; }
+    public string Sposob { get; set; }
 }
